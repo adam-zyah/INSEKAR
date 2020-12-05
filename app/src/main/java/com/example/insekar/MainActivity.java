0ba7384a1b97f4370de0e30c29030c859a1011fd
zamnarofiq1 <zamnarofiq123@gmail.com>
2020-12-05 19:39:37 +0700

    Mendesain tampilan utama

--- app/src/main/java/com/example/insekar/MainActivity.java
+++ app/src/main/java/com/example/insekar/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.insekar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void puskesmas(View view) {
+    }
+
+    public void pendaftaran(View view) {
+    }
+
+    public void antrian(View view) {
+    }
+
+    public void tesmata(View view) {
+    }
+
+    public void info(View view) {
+    }
+
+    public void maps(View view) {
     }
 }
