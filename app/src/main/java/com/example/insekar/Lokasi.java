0303f2a2f0b119ef3534f7e54ceee5baa9323af0
AdamZyah <pahleviap123@gmail.com>
2020-12-16 08:41:06 -0800

    membuat lokasi puskesmas

--- app/src/main/java/com/example/insekar/Lokasi.java
+++ app/src/main/java/com/example/insekar/Lokasi.java
@@ -62,5 +62,67 @@ public class Lokasi extends FragmentActivity implements OnMapReadyCallback {
         LatLng BanturadenII = new LatLng(-7.361701914556777, 109.23689961778354);
         mMap.addMarker(new MarkerOptions().position(BanturadenII).title("Puskesmas Banturaden II"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(BanturadenII));
+
+        // Puskesmas Cilongok I
+        LatLng CilongokI = new LatLng(-7398700, 109.123438);
+        mMap.addMarker(new MarkerOptions().position(CilongokI).title("Puskesmas Cilongok I"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(CilongokI));
+
+        // Puskesmas Cilongok II
+        LatLng CilongokII = new LatLng(-7.446471, 109.160355);
+        mMap.addMarker(new MarkerOptions().position(CilongokI).title("Puskesmas Cilongok II"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(CilongokII));
+
+        // Puskesmas Gumelar
+        LatLng Gumelar = new LatLng(-7.373670, 108.981425);
+        mMap.addMarker(new MarkerOptions().position(Gumelar).title("Puskesmas Gumelar"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Gumelar));
+
+        // Puskesmas II Tambak
+        LatLng IITambak = new LatLng(-7.612881, 109.413663);
+        mMap.addMarker(new MarkerOptions().position(IITambak).title("Puskesmas II Tambak"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(IITambak));
+
+        // Puskesmas Jatilawang
+        LatLng Jatilawang = new LatLng(-7.543392, 109.121674);
+        mMap.addMarker(new MarkerOptions().position(Jatilawang).title("Puskesmas Jatilawang"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Jatilawang));
+
+        // Puskesmas Kalibagor
+        LatLng Kalibagor = new LatLng(-7.473552, 109.297506);
+        mMap.addMarker(new MarkerOptions().position(Kalibagor).title("Puskesmas Kalibagor"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Kalibagor));
+
+        // Puskesmas Karang Lewas
+        LatLng KarangLewas = new LatLng(-7.416744, 109.183888);
+        mMap.addMarker(new MarkerOptions().position(KarangLewas).title("Puskesmas Karang Lewas"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(KarangLewas));
+
+        // Puskesmas Kebasen
+        LatLng Kebasen = new LatLng(-7.534175, 109.198043);
+        mMap.addMarker(new MarkerOptions().position(Kebasen).title("Puskesmas Kebasen"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Kebasen));
+
+        // Puskesmas Kedung Banteng
+        LatLng KedungBanteng = new LatLng(-7.392801, 109.199927);
+        mMap.addMarker(new MarkerOptions().position(KedungBanteng).title("Puskesmas Kedung Banteng"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(KedungBanteng));
+
+        // Puskesmas Kembaran I
+        LatLng KembaranI = new LatLng(-7.400447, 109.285341);
+        mMap.addMarker(new MarkerOptions().position(KembaranI).title("Puskesmas Kembaran I"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(KembaranI));
+
+        // Puskesmas Kemrajen I
+        LatLng KemrajenI = new LatLng(-7.591420, 109.291599);
+        mMap.addMarker(new MarkerOptions().position(KemrajenI).title("Puskesmas Kemrajen I"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(KemrajenI));
+
+        // Puskesmas Kemrajen II
+        LatLng KemrajenII = new LatLng(-7.592496, 109.272324);
+        mMap.addMarker(new MarkerOptions().position(KemrajenII).title("Puskesmas Kemrajen II"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(KemrajenII));
+
+
     }
 }
